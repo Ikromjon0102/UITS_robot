bf155fa1b2a74dafdd1c01a4ec5177edb418ee79
Ikromjon <80442693+Ikromjon0102@users.noreply.github.com>
2022-04-25 05:44:57 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,15 +1,16 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-# import os
-# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-# ADMINS = str(os.environ.get("ADMINS"))
-# IP = str(os.environ.get("ip"))
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
+import os
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = str(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
 
